618ad85eaa8e9f99b659fa03670dae339f164ffa
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2024-02-28 12:31:02 +0530

    Added interactive color picker using OpenCV trackbars
    Description:
    - Created a simple OpenCV program to demonstrate real-time color adjustments using trackbars.
    - The user can interactively modify the intensity of the Blue (B), Green (G), and Red (R) channels.
    - The `nothing` function is used as a callback when trackbar values change.
    - Pressing the 'Esc' key exits the program.

--- Trackbar.py
+++ Trackbar.py
@@ -39,3 +39,37 @@ while True:
 
 # Close all OpenCV windows
 cv2.destroyAllWindows()
+
+
+"""-----------------------------------------------------------------------Added few things in below Code----------------------------------------------------------------------------"""
+import cv2
+import numpy as np
+
+def nothing(x):
+    print(x)
+
+img = np.zeros ((300, 512, 3), np.uint8)
+cv2.namedWindow('image')
+
+cv2.createTrackbar('B', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
+
+switch = '0 : off\n 1: oN'
+cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+while True:
+    cv2.imshow('image', img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    b = cv2.getTrackbarPos('B', 'image')
+    g = cv2.getTrackbarPos('G', 'image')
+    r = cv2.getTrackbarPos('R', 'image')
+    s = cv2.getTrackbarPos(switch, 'image')
+    if s == 0:
+        img[:] = 0
+    else:
+        img[:] = [b, g, r]
+
+cv2.destroyAllWindows()
